f18b7bf0dc45c4c45a57c88faa268cade6182ca9
Kyle Gardner <kb_gardner@icloud.com>
2020-02-03 08:18:59 -0700

    commit from kyle gardner

--- HelloFromScott/src/currell/Main.java
+++ HelloFromScott/src/currell/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Scott.");
     }
+    public static void main(String[] args) {
+        System.out.println("Hello Scott, my name is Kyle.");
+    }
+
 }
