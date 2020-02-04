0e8eb8b0efded4c13211c26f5cd179d46b1c0e0b
scott <scottcurrell@gmail.com>
2020-02-03 21:21:42 -0700

    remove duplicate main function

--- HelloFromScott/src/currell/Main.java
+++ HelloFromScott/src/currell/Main.java
@@ -3,8 +3,6 @@ package currell;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Scott.");
-    }
-    public static void main(String[] args) {
         System.out.println("Hello Scott, my name is Kyle.");
     }
 
