a7e640827cdcc3d82efa509585e6edd182d52927
scott <scottcurrell@gmail.com>
2020-03-09 19:07:17 -0600

    remove old work

--- HelloFromScott/src/currell/Main.java
+++
@@ -1,9 +0,0 @@
-package currell;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, my name is Scott.");
-        System.out.println("Hello Scott, my name is Kyle.");
-    }
-
-}
