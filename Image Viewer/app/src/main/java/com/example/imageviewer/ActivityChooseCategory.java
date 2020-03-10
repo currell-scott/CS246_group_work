e281b9c98b71d655d20c0bfb121f6355c89aca1f
scott <scottcurrell@gmail.com>
2020-03-09 23:25:35 -0600

    create choose category placeholder activity

---
+++ Image Viewer/app/src/main/java/com/example/imageviewer/ActivityChooseCategory.java
@@ -0,0 +1,22 @@
+package com.example.imageviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivityChooseCategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_category);
+    }
+
+    /** Called when the user taps the 'Home' button */
+    public void returnHome(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+        startActivity(intent);
+    }
+}
