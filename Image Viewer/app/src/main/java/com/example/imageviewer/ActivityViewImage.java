92e55fd77eab0cabaadf15a0a0ee1c7b03a62e39
scott <scottcurrell@gmail.com>
2020-03-14 21:16:19 -0600

    skeleton views built, navigation between views working, placeholder images added

--- Image Viewer/app/src/main/java/com/example/imageviewer/ActivityViewImage.java
+++ Image Viewer/app/src/main/java/com/example/imageviewer/ActivityViewImage.java
@@ -1,28 +1,22 @@
 package com.example.imageviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class ActivityDisplayCategory extends AppCompatActivity {
+public class ActivityViewImage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_category);
+        setContentView(R.layout.activity_view_image);
     }
 
     /** Called when the user taps the 'Home' button */
     public void returnHome(View view) {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-
-        startActivity(intent);
-    }
-
-    /** Called when the user taps the 'Home' button */
-    public void viewImage(View view) {
-        Intent intent = new Intent(getApplicationContext(), ActivityViewImage.class);
 
         startActivity(intent);
     }
